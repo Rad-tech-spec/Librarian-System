129717a17ef02af3355fb729ad45b1bd78fb3c14
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-03-05 19:44:59 +0200

    DBConnector setup

--- config/DBConnector.java
+++ config/DBConnector.java
@@ -1,5 +1,9 @@
 package config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * 
  * Class that handles connection to and manipulation of the database.
@@ -7,10 +11,33 @@ package config;
  */
 class DBConnector {
 	
-	public DBConnector()
-	{
-		
+	private Connection connection;
+	private String jdbcURL = "jdbc:postgresql://ziggy.db.elephantsql.com:5432/efcagywl";
+	private String username = "efcagywl";
+	private String password = "PMMtt1RExmvYJXt37yaT0qxi5XQI5fci";
+	
+	public DBConnector() {
+		connect();
 	}
 	
+	public boolean connect() {
+		try {
+			connection = DriverManager.getConnection(jdbcURL, username, password);
+			System.out.println("Connected to PostgreSQL server.");
+			return true;
+		}
+		catch (SQLException e) {
+			return false;
+		}
+	}
 	
+	public boolean disconnect() {
+		try {
+			connection.close();
+			return true;
+		}
+		catch (SQLException e) {
+			return false;
+		}
+	}
 }
