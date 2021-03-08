65c54f2536467b19c9443c80762f311a186efd00
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-03-08 10:32:28 +0200

    created separate folder for assignment1package

---
+++ assignment1package/Main.java
@@ -0,0 +1,24 @@
+package assignment1package;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		GridPane login = Login.loginMenu();
+
+		Scene scene = new Scene(login, 600, 600);
+		primaryStage.setTitle("Library Managment System");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+}
