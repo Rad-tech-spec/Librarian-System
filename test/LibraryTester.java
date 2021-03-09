e07e05f87aff9676dcc3777e97c525f3bfc8a0dd
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-03-09 20:48:43 +0200

    new test case added

--- test/LibraryTester.java
+++ test/LibraryTester.java
@@ -2,16 +2,24 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import dao.DBController;
 import model.ItemSearchAttribute;
 
+/**
+ * This class performs unit tests for the <i>Library Application</i>.
+ * 
+ * @author Nikita Mezhenskyi
+ */
 class LibraryTester {
 	private final DBController db = new DBController();
 
 	@ParameterizedTest
+	@DisplayName("Get Items by Title - valid titles")
 	@ValueSource(strings = {"The Irishman", "Irishman", "irishman", "irish"})
 	void testGetItemsByTitle(String searchValue) {
 		assertTrue(db.getItemsByAttribute(ItemSearchAttribute.TITLE, "The Irishman") != null && 
@@ -19,6 +27,7 @@ class LibraryTester {
 	}
 	
 	@ParameterizedTest
+	@DisplayName("Get Items by Author - valid author names")
 	@ValueSource(strings = {"Marcus Aurelius", "marcus aurelius", "marcus", "aurelius"})
 	void testGetItemsByAuthor(String searchValue) {
 		assertTrue(db.getItemsByAttribute(ItemSearchAttribute.AUTHOR, searchValue) != null &&
@@ -26,9 +35,16 @@ class LibraryTester {
 	}
 
 	@ParameterizedTest
+	@DisplayName("Get Items by ID - valid IDs")
 	@ValueSource(strings = {"122", "150", "123"})
 	void testGetItemsById(String searchValue) {
 		assertTrue(db.getItemsByAttribute(ItemSearchAttribute.ID, searchValue) != null &&
 				!db.getItemsByAttribute(ItemSearchAttribute.ID, searchValue).isEmpty());
+	}
+	
+	@Test
+	@DisplayName("Get Borrowed Items - invalid Student ID")
+	void testGetBorrowedItems() {
+		assertTrue(db.getBorrowedItems("100200300", true).isEmpty());
 	}
 }
