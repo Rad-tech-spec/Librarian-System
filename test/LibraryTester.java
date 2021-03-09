96c17ac88b334d921c1cf5a4746a26dcdc7ff933
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2021-03-09 18:04:29 +0200

    added test for DBController.getItemsByAttribute

--- test/LibraryTester.java
+++ test/LibraryTester.java
@@ -2,7 +2,8 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import dao.DBController;
 import model.ItemSearchAttribute;
@@ -10,9 +11,24 @@ import model.ItemSearchAttribute;
 class LibraryTester {
 	private final DBController db = new DBController();
 
-	@Test
-	void testGetItemsByAuthor() {
-		assertEquals(true, db.getItemsByAttribute(ItemSearchAttribute.AUTHOR, "Dan") != null);
+	@ParameterizedTest
+	@ValueSource(strings = {"The Irishman", "Irishman", "irishman", "irish"})
+	void testGetItemsByTitle(String searchValue) {
+		assertTrue(db.getItemsByAttribute(ItemSearchAttribute.TITLE, "The Irishman") != null && 
+				!db.getItemsByAttribute(ItemSearchAttribute.TITLE, "The Irishman").isEmpty());
+	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"Marcus Aurelius", "marcus aurelius", "marcus", "aurelius"})
+	void testGetItemsByAuthor(String searchValue) {
+		assertTrue(db.getItemsByAttribute(ItemSearchAttribute.AUTHOR, searchValue) != null &&
+				!db.getItemsByAttribute(ItemSearchAttribute.AUTHOR, searchValue).isEmpty());
 	}
 
+	@ParameterizedTest
+	@ValueSource(strings = {"122", "150", "123"})
+	void testGetItemsById(String searchValue) {
+		assertTrue(db.getItemsByAttribute(ItemSearchAttribute.ID, searchValue) != null &&
+				!db.getItemsByAttribute(ItemSearchAttribute.ID, searchValue).isEmpty());
+	}
 }
